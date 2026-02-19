5f0cdc74426191a69be979a2f54ed165ae1a954c
Aaron Strong <aaronstrong100@gmail.com>
2026-02-18 19:06:29 -0700

    Created and created method signatures for GameService class

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,20 @@
+package service;
+
+import Requests.CreateGameRequest;
+import Requests.JoinGameRequest;
+import Requests.ListGamesRequest;
+import Results.CreateGameResult;
+import Results.JoinGameResult;
+import Results.ListGamesResult;
+
+public class GameService {
+    public ListGamesResult listGames(ListGamesRequest gameListRequest){
+        return null;
+    }
+    public CreateGameResult createGame(CreateGameRequest createGameRequest){
+        return null;
+    }
+    public JoinGameResult joinGame(JoinGameRequest joinGameRequest) {
+        return null;
+    }
+}
