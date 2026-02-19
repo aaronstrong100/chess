8cf28e3f334e8909a04a1c2c103f30b263e6da66
Aaron Strong <aaronstrong100@gmail.com>
2026-02-18 20:25:31 -0700

    Created shells for Handler classes

---
+++ server/src/main/java/Handlers/CreateGameHandler.java
@@ -0,0 +1,17 @@
+package Handlers;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+public class CreateGameHandler implements Handler {
+    /**
+     *
+     * @param context
+     * @throws Exception
+     */
+    @Override
+    public void handle(@NotNull Context context){
+
+    }
+}
---
+++ server/src/main/java/Handlers/LoginHandler.java
@@ -0,0 +1,16 @@
+package Handlers;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+public class LoginHandler implements Handler{
+    /**
+     *
+     * @param context
+     * @throws Exception
+     */
+    @Override
+    public void handle(@NotNull Context context){
+
+    }
+}
