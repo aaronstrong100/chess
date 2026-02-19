ee6683282b72c970ef8c1ad125b95d2fffda873c
Aaron Strong <aaronstrong100@gmail.com>
2026-02-19 08:54:15 -0700

    Added test handler to Server for registering

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -4,6 +4,13 @@ import io.javalin.*;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.javalin.json.JsonMapper;
+import org.jetbrains.annotations.NotNull;
+import java.lang.reflect.Type;
+
+import java.util.Map;
 
 public class Server {
 
@@ -13,7 +20,7 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
-        javalin.get("/register", new RegisterHandler());
+        javalin.get("/game", new RegisterHandler());
     }
 
     public int run(int desiredPort) {
@@ -33,7 +40,9 @@ public class Server {
          */
         @Override
         public void handle(@NotNull Context context){
-
+            Map<String, String> output = Map.of("title","Registering...");
+            Gson gson = new Gson();
+            context.json(gson.toJson(output,output.getClass()));
         }
     }
 }
