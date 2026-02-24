52c08992b4c6b5ca78eb76c98d6965cab0e0bbe0
Aaron Strong <aaronstrong100@gmail.com>
2026-02-24 16:54:15 -0700

    Implemented UserService for the Register endpoint

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -5,15 +5,31 @@ import Requests.LogoutRequest;
 import Requests.RegisterRequest;
 import Results.LoginResult;
 import Results.RegisterResult;
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
 
 public class UserService {
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+    public UserService(){
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+    }
     /**
      * register a new user
      * @param registerRequest RegisterRequest object containing the username and password of the new user
      * @return a RegisterResult Object containing the new authToken for the user
      */
-    public RegisterResult register(RegisterRequest registerRequest){
-        return null;
+    public RegisterResult register(RegisterRequest registerRequest) throws Exception{
+        try{
+            userDAO.getUserData(registerRequest.getUsername());
+            throw new Exception("The username is already taken");
+        } catch(DataAccessException e){
+            userDAO.addUserData(new UserData(registerRequest.getUsername(), registerRequest.getPassword(), registerRequest.getEmail()));
+            AuthData returnData = authDAO.addAuthData(new AuthData(registerRequest.getUsername(), authDAO.generateNewAuthToken()));
+            return new RegisterResult(returnData.getUsername(), returnData.getAuthToken());
+        }
     }
 
     /**
