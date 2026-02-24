8d03a4f1cbb055d90b2b439d9aedc8593916ff83
Aaron Strong <aaronstrong100@gmail.com>
2026-02-24 16:50:07 -0700

    Edited the order of arguments in AuthData construction

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -3,7 +3,7 @@ package model;
 public class AuthData {
     private final String authToken;
     private final String username;
-    public AuthData(String authToken, String username){
+    public AuthData(String username, String authToken){
         this.authToken = authToken;
         this.username = username;
     }
