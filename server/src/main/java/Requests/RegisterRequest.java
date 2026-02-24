6efc871a7e5692e9b2b72df606cd775c7dad0f4e
Aaron Strong <aaronstrong100@gmail.com>
2026-02-24 12:02:50 -0700

    Implemented RegisterRequest

--- server/src/main/java/Requests/RegisterRequest.java
+++ server/src/main/java/Requests/RegisterRequest.java
@@ -1,4 +1,21 @@
 package Requests;
 
 public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+    public RegisterRequest(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public String getEmail(){
+        return this.email;
+    }
 }
