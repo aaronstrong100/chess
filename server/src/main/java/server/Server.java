27b87da343fb7e01c863fe5c3c46f641c9fc9b28
Aaron Strong <aaronstrong100@gmail.com>
2026-02-26 16:04:43 -0700

    Implemented Handler for Login endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,13 @@
 package server;
 
+import Requests.LoginRequest;
 import Requests.RegisterRequest;
+import Results.LoginResult;
 import Results.RegisterResult;
 import com.google.gson.*;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
 import io.javalin.*;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
@@ -10,6 +15,8 @@ import org.jetbrains.annotations.NotNull;
 import io.javalin.json.JsonMapper;
 import org.jetbrains.annotations.NotNull;
 import service.UserService;
+import service.GameService;
+import service.DeleteService;
 
 import java.lang.reflect.Type;
 
@@ -18,13 +25,24 @@ import java.util.Map;
 public class Server {
 
     private final Javalin javalin;
-    protected UserService userService;
+
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+
+    private UserService userService;
+    private GameService gameService;
+    private DeleteService deleteService;
+
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
-        userService = new UserService();
+        userService = new UserService(userDAO, authDAO);
+
         javalin.post("/user", new RegisterHandler(userService));
+
+        javalin.post("/session", new LoginHandler(userService));
     }
 
     public int run(int desiredPort) {
@@ -52,6 +70,29 @@ public class Server {
             try {
                 RegisterResult registerResult = userService.register(registerRequest);
                 context.json(gson.toJson(registerResult));
+            } catch (Exception e){
+                context.json("{\"message\": \""+ e.getMessage() + "\"}");
+            }
+        }
+    }
+
+
+    public static class LoginHandler implements Handler {
+
+        UserService userService;
+
+        public LoginHandler(UserService userService){
+            this.userService = userService;
+        }
+
+        @Override
+        public void handle(@NotNull Context context){
+            Gson gson = new Gson();
+            String jsonString = context.body();
+            LoginRequest loginRequest = gson.fromJson(jsonString, LoginRequest.class);
+            try {
+                LoginResult loginResult = userService.login(loginRequest);
+                context.json(gson.toJson(loginResult));
             } catch (Exception e){
                 context.json("{\"message\": \""+ e.getMessage() + "\"}");
             }
