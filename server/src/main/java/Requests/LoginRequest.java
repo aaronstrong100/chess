f9d4c78b4dd8abd43e28642724ef03141253af8c
Aaron Strong <aaronstrong100@gmail.com>
2026-02-26 16:02:32 -0700

    Implemented LoginRequest

--- server/src/main/java/Requests/LoginRequest.java
+++ server/src/main/java/Requests/LoginRequest.java
@@ -1,4 +1,16 @@
 package Requests;
 
 public class LoginRequest {
+    private String username;
+    private String password;
+    public LoginRequest(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){
+        return this.password;
+    }
 }
