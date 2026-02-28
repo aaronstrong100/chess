dc6655c271d925caacf520302391dc2eae6bd007
Aaron Strong <aaronstrong100@gmail.com>
2026-02-28 11:53:40 -0700

    Implemented GameService listGames

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -6,10 +6,22 @@ import Requests.ListGamesRequest;
 import Results.CreateGameResult;
 import Results.JoinGameResult;
 import Results.ListGamesResult;
+import dataaccess.GameDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.AuthDAO;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.DataAccessException;
 
 public class GameService {
-    public ListGamesResult listGames(ListGamesRequest gameListRequest){
-        return null;
+    private GameDAO gameDAO;
+    private AuthDAO authDAO;
+    public GameService(GameDAO gameDAO, AuthDAO authDAO){
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+    public ListGamesResult listGames(ListGamesRequest gameListRequest) throws DataAccessException{
+        this.authDAO.getAuthData(gameListRequest.getAuthToken());
+        return new ListGamesResult(this.gameDAO.getCurrentGames());
     }
     public CreateGameResult createGame(CreateGameRequest createGameRequest){
         return null;
