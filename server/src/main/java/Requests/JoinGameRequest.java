9daa070b4f01f3db2eda3b74638e930294f6550f
Aaron Strong <aaronstrong100@gmail.com>
2026-02-28 11:07:51 -0700

    Implemented JoinGameRequest POJA

--- server/src/main/java/Requests/JoinGameRequest.java
+++ server/src/main/java/Requests/JoinGameRequest.java
@@ -1,4 +1,21 @@
 package Requests;
 
 public class JoinGameRequest {
+    private String playerColor;
+    private String gameID;
+    private String authToken;
+    public JoinGameRequest(String playerColor, String gameID, String authToken){
+        this.playerColor = playerColor;
+        this.gameID = gameID;
+        this.authToken = authToken;
+    }
+    public String getPlayerColor(){
+        return this.playerColor;
+    }
+    public String getGameID(){
+        return this.gameID;
+    }
+    public String getAuthToken(){
+        return this.authToken;
+    }
 }
