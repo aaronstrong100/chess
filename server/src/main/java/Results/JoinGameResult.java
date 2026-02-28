a184fb2ec9883e7074e53a768c51dc3c0a045341
Aaron Strong <aaronstrong100@gmail.com>
2026-02-28 11:08:00 -0700

    Implemented JoinGameResult POJA

--- server/src/main/java/Results/JoinGameResult.java
+++ server/src/main/java/Results/JoinGameResult.java
@@ -1,4 +1,16 @@
 package Results;
 
 public class JoinGameResult {
+    private String playerColor;
+    private String gameID;
+    public JoinGameResult(String playerColor, String gameID){
+        this.playerColor = playerColor;
+        this.gameID = gameID;
+    }
+    public String getPlayerColor(){
+        return this.playerColor;
+    }
+    public String getGameID(){
+        return this.gameID;
+    }
 }
