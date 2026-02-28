02ff22e7e8e07593623a8fc04507c226ca3f493e
Aaron Strong <aaronstrong100@gmail.com>
2026-02-28 11:08:15 -0700

    Implemented ListGamesResult POJA

--- server/src/main/java/Results/ListGamesResult.java
+++ server/src/main/java/Results/ListGamesResult.java
@@ -1,4 +1,13 @@
 package Results;
 
+import chess.ChessGame;
+
 public class ListGamesResult {
+    private ChessGame[] games;
+    public ListGamesResult(ChessGame[] games){
+        this.games = games;
+    }
+    public ChessGame[] getGames(){
+        return this.games;
+    }
 }
