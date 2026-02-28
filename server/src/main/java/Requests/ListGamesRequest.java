67b467fa852f473dc7403135fbd931cbd6668887
Aaron Strong <aaronstrong100@gmail.com>
2026-02-28 11:08:09 -0700

    Implemented ListGamesRequest POJA

--- server/src/main/java/Requests/ListGamesRequest.java
+++ server/src/main/java/Requests/ListGamesRequest.java
@@ -1,4 +1,11 @@
 package Requests;
 
 public class ListGamesRequest {
+    private String authToken;
+    public ListGamesRequest(String authToken){
+        this.authToken = authToken;
+    }
+    public String getAuthToken(){
+        return this.authToken;
+    }
 }
