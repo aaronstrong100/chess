98015845d9740a0baa8d710d5f9f85695dedd418
Aaron Strong <aaronstrong100@gmail.com>
2026-02-28 11:07:42 -0700

    Implemented DeleteRequest POJA

--- server/src/main/java/Requests/DeleteRequest.java
+++ server/src/main/java/Requests/DeleteRequest.java
@@ -1,4 +1,11 @@
 package Requests;
 
 public class DeleteRequest {
+    private String authToken;
+    public DeleteRequest(String authToken){
+        this.authToken = authToken;
+    }
+    public String getAuthToken(){
+        return this.authToken;
+    }
 }
