764fcce1925b9ca2eaffe972d62d3f31e2afc55a
Aaron Strong <aaronstrong100@gmail.com>
2026-02-28 11:07:24 -0700

    Implemented CreateGameRequest POJA

--- server/src/main/java/Requests/CreateGameRequest.java
+++ server/src/main/java/Requests/CreateGameRequest.java
@@ -1,4 +1,16 @@
 package Requests;
 
 public class CreateGameRequest {
+    private String gameName;
+    private String authToken;
+    public CreateGameRequest(String gameName, String authToken){
+        this.gameName = gameName;
+        this.authToken = authToken;
+    }
+    public String getGameName(){
+        return this.gameName;
+    }
+    public String getAuthToken(){
+        return this.authToken;
+    }
 }
