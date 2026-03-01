cf94e542587cbc22277eb68292fe75940a129374
Aaron Strong <aaronstrong100@gmail.com>
2026-02-28 23:27:09 -0700

    implemented DeleteService

--- server/src/main/java/service/DeleteService.java
+++ server/src/main/java/service/DeleteService.java
@@ -1,9 +1,22 @@
 package service;
 
 import Requests.DeleteRequest;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
 
 public class DeleteService {
-    public void delete(DeleteRequest deleteRequest){
-
+    private GameDAO gameDAO;
+    private AuthDAO authDAO;
+    private UserDAO userDAO;
+    public DeleteService(GameDAO gameDAO, AuthDAO authDAO, UserDAO userDAO){
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+    }
+    public void delete(){
+        this.gameDAO.clearDataBase();
+        this.authDAO.clearDataBase();
+        this.userDAO.clearDataBase();
     }
 }
