35db1aa7f5f6c05a95ced4775b812f9df740bb33
Aaron Strong <aaronstrong100@gmail.com>
2026-02-28 22:10:42 -0700

    implemented GameService joinGame method

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -12,6 +12,8 @@ import dataaccess.AuthDAO;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.DataAccessException;
 
+import model.GameData;
+
 public class GameService {
     private GameDAO gameDAO;
     private AuthDAO authDAO;
@@ -27,7 +29,25 @@ public class GameService {
         this.authDAO.getAuthData(createGameRequest.getAuthToken());
         return new CreateGameResult(this.gameDAO.createGame(createGameRequest.getGameName()));
     }
-    public JoinGameResult joinGame(JoinGameRequest joinGameRequest) {
-        return null;
+    public JoinGameResult joinGame(JoinGameRequest joinGameRequest) throws DataAccessException, Exception{
+        String username = this.authDAO.getAuthData(joinGameRequest.getAuthToken()).getUsername();
+        GameData gameData = this.gameDAO.getGame(joinGameRequest.getGameID());
+        if(joinGameRequest.getPlayerColor().equalsIgnoreCase("WHITE")){
+            if(gameData.getWhiteUsername()=="None"){
+                this.gameDAO.overwriteGame(joinGameRequest.getGameID(),gameData.updateWhiteUsername(username));
+                return new JoinGameResult(joinGameRequest.getPlayerColor(), joinGameRequest.getGameID());
+            }else {
+                throw new Exception("Team WHITE is already taken");
+            }
+        } else if (joinGameRequest.getPlayerColor().equalsIgnoreCase("BLACK")){
+            if(gameData.getBlackUsername()=="None"){
+                this.gameDAO.overwriteGame(joinGameRequest.getGameID(),gameData.updateBlackUsername(username));
+                return new JoinGameResult(joinGameRequest.getPlayerColor(), joinGameRequest.getGameID());
+            }else {
+                throw new Exception("Team BLACK is already taken");
+            }
+        } else {
+            throw new Exception("Value of playerColor must be WHITE or BLACK");
+        }
     }
 }
