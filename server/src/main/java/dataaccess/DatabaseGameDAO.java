6fa1b0eaae18ddba9a94a38af39b0de1a4df61fa
Aaron Strong <aaronstrong100@gmail.com>
2026-03-04 11:26:14 -0700

    Created class DatabaseGameDAO with method outlines

---
+++ server/src/main/java/dataaccess/DatabaseGameDAO.java
@@ -0,0 +1,30 @@
+package dataaccess;
+
+import java.util.ArrayList;
+
+public class DatabaseGameDAO implements GameDAO{
+    @Override
+    public ArrayList<GameData> getCurrentGames() {
+        return null;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public int createGame(String gameName) {
+        return 0;
+    }
+
+    @Override
+    public void overwriteGame(int gameID, GameData updatedGame) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clearDataBase() {
+
+    }
+}
