0d0c608de23d4aa182c01fec6f117bd4f1f527ad
Aaron Strong <aaronstrong100@gmail.com>
2026-03-04 20:48:57 -0700

    Created getUserData method for MySqlUserDAO

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -6,12 +6,23 @@ public class MySqlUserDAO implements UserDAO{
 
     @Override
     public UserData getUserData(String username) throws UnauthorizedException{
-        return null;
+        var conn = DatabaseManger.getConnection();
+        var getUserStatement = "SELECT username, password, email FROM user_data WHERE username="+username+";";
+        try(var preparedGetUserStatement = conn.prepareStatement(getUserStatement)){
+             getUserStatement.setString(1,username);
+             try(var rs = preparedGetUserStatement.executeQuery()){
+                 if(rs.next()){
+                     String usernameData = rs.getString("username");
+                     String passwordData = rs.getString("password");
+                     String emailData = rs.getString("email");
+                     return new UserData(usernameData, passwordData, emailData);
+                 }
+             }
+        }
     }
 
     @Override
     public void addUserData(UserData userData){
-
     }
 
     @Override
