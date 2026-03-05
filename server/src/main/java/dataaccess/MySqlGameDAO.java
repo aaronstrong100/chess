af94a9e0b5d7fddefd6000ac57712a01143e26a7
Aaron Strong <aaronstrong100@gmail.com>
2026-03-05 13:37:37 -0700

    implemented createGame in MySqlGameDAO and passed unit tests

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -1,10 +1,18 @@
 package dataaccess;
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.*;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MySqlGameDAO implements GameDAO{
+    private int nextID;
+
+    public MySqlGameDAO(){
+        nextID = 0;
+    }
+
     @Override
     public ArrayList<GameData> getCurrentGames() {
         return null;
@@ -15,9 +23,29 @@ public class MySqlGameDAO implements GameDAO{
         return null;
     }
 
+    private int getNextID(){
+        nextID++;
+        return nextID;
+    }
+
     @Override
     public int createGame(String gameName) {
-        return 0;
+        GameData gameData = new GameData(getNextID(), null, null, gameName, new ChessGame());
+        Gson gson = new Gson();
+        try (var conn = DatabaseManager.getConnection()) {
+            var addUserStatement = "INSERT INTO game_data (gameID, white_username, black_username, game_name, chess_game) VALUES(?,?,?,?,?)";
+            try (var preparedAddUserStatement = conn.prepareStatement(addUserStatement)) {
+                preparedAddUserStatement.setInt(1, gameData.getGameID());
+                preparedAddUserStatement.setString(2, gameData.getWhiteUsername());
+                preparedAddUserStatement.setString(3, gameData.getBlackUsername());
+                preparedAddUserStatement.setString(4, gameData.getGameName());
+                preparedAddUserStatement.setString(5, gson.toJson(gameData.getGame()));
+                preparedAddUserStatement.executeUpdate();
+                return gameData.getGameID();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Override
