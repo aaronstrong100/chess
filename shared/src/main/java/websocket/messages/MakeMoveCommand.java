b2f838c8ba8d1e84335ee699dd7a587a030d603a
Aaron Strong <aaronstrong100@gmail.com>
2026-03-28 18:06:01 -0600

    Created MakeMoveCommand

---
+++ shared/src/main/java/websocket/messages/MakeMoveCommand.java
@@ -0,0 +1,40 @@
+package websocket.messages;
+
+import chess.ChessMove;
+import websocket.commands.UserGameCommand;
+
+import java.util.Objects;
+
+public class MakeMoveCommand extends UserGameCommand {
+
+    private ChessMove move;
+
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
+        super(commandType, authToken, gameID);
+        this.move = move;
+    }
+
+
+    public ChessMove getMove(){
+        return this.move;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + move.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MakeMoveCommand that)) {
+            return false;
+        }
+        return Objects.equals(this.getMove(), that.getMove());
+    }
+}
